7d8efd36f81cb6210dc0f65af6ce9f7e8914deb9
Linda <jeropkoimur@gmail.com>
2025-10-26 01:02:02 +0300

    message

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am a passionate web developer who loves creating beautiful and functional websites. I enjoy learning new technologies and solving complex problems through code.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
